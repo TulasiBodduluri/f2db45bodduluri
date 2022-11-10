adcf10098c2c2dc5c16457be770b6f51b1cdd656
TulasiBodduluri <tulasiboddu10@gmail.com>
2022-11-09 21:33:34 -0600

    added the new content

--- controllers/employee.js
+++ controllers/employee.js
@@ -10,6 +10,19 @@ exports.employee_list = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 };
+
+// VIEWS 
+// Handle a show all view 
+exports.employee_view_all_Page = async function(req, res) { 
+    try{ 
+        theEmployees = await Employee.find(); 
+        res.render('employee', { title: 'Employee Search Results', results: theEmployees }); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
  
 // for a specific Costume. 
 exports.employee_detail = function(req, res) { 
@@ -17,8 +30,28 @@ exports.employee_detail = function(req, res) {
 }; 
  
 // Handle Costume create on POST. 
-exports.employee_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Employee create POST'); 
+//exports.employee_create_post = function(req, res) { 
+    //res.send('NOT IMPLEMENTED: Employee create POST'); 
+//};
+
+exports.employee_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new Employee(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    document.ID = req.body.ID; 
+    document.age = req.body.age; 
+    document.salary = req.body.salary; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
 // Handle Costume delete form on DELETE.
--- routes/employee.js
+++ routes/employee.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+//var express = require('express');
+//var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('employee', { title: 'Search Results Employee' });
-});
+//router.get('/', function(req, res, next) {
+  //res.render('employee', { title: 'Search Results Employee' });
+//});
 
-module.exports = router;
+//module.exports = router;
+
+var express = require('express'); 
+const employee_controlers= require('../controllers/employee'); 
+var router = express.Router(); 
+ 
+/* GET costumes */ 
+router.get('/', employee_controlers.employee_view_all_Page ); 
+module.exports = router; 
