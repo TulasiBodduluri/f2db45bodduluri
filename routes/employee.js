88083719c9ce8a2d96f11c4d95419beb97c90274
TulasiBodduluri <tulasiboddu10@gmail.com>
2022-11-18 10:08:53 -0600

    added required changes

--- routes/employee.js
+++ routes/employee.js
@@ -13,12 +13,12 @@ const employee_controlers= require('../controllers/employee');
 var router = express.Router(); 
  
 /* GET costumes */ 
-//router.get('/', employee_controlers.employee_view_all_Page ); 
+router.get('/', employee_controlers.employee_view_all_Page ); 
 /* GET detail costume page */
 /* GET create costume page */ 
-//router.get('/create', employee_controlers.employee_create_Page);  
+router.get('/create', employee_controlers.employee_create_Page);  
 router.get('/detail', employee_controlers.employee_view_one_Page);
 /* GET create update page */ 
-//router.get('/update', employee_controlers.employee_update_Page);  
-//router.get('/delete', employee_controlers.employee_delete_Page); 
+router.get('/update', employee_controlers.employee_update_Page);  
+router.get('/delete', employee_controlers.employee_delete_Page); 
 module.exports = router; 
